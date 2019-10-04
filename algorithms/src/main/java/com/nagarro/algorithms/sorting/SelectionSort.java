af719de9d724867dd1dc893439d2ecbfb85609dc
jobanpreetsingh <jobanpreetsingh@192.168.1.6>
2019-10-04 09:29:23 +0530

    Fixed Selection Sorting.

--- algorithms/src/main/java/com/nagarro/algorithms/sorting/SelectionSort.java
+++ algorithms/src/main/java/com/nagarro/algorithms/sorting/SelectionSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public final class SelectionSort {
 
     public static void main(final String[] args) {
-        final int[] array = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        final int[] array = { 9, 8, 7, 6, 5, 4, 3, 2, 0, 1 };
         System.out.println("Before Sorting :");
         Arrays.stream(array).forEach(System.out::print);
         System.out.println();
@@ -18,7 +18,7 @@ public final class SelectionSort {
         for (int i = 0; i < totalPass; i++) {
             int minIndex = i;
             for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[i]) {
+                if (array[j] < array[minIndex]) {
                     minIndex = j;
                 }
             }
